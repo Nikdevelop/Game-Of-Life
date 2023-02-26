8ab7ebea6c315b55c4b0328fccc9e1fa61e58a63
Никита <42384037+Nikdevelop@users.noreply.github.com>
2023-02-26 21:29:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import pygame
+from random import choices
+import utils
+import time
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+CELL_ALIVE = (255, 108, 63)
+CELL_DEAD = (0, 0, 0)
+GRID = (50, 50, 50)
+
+# Parameters
+WIDTH, HEIGHT = 1000, 500
+CELL_SIZE = 5
+GRID_THICKNESS = 1
+FPS = 30
+RANDOMIZE = True
+EDITOR_MODE = True
+
+cell_x_cnt, cell_y_cnt = round(WIDTH / CELL_SIZE), round(HEIGHT / CELL_SIZE)
+grid = [choices([0, 1], k=cell_x_cnt) if RANDOMIZE else [0] * cell_x_cnt for _ in range(cell_y_cnt)]
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption("Game Of Life")
+clock = pygame.time.Clock()
+
+utils.fill_grid(screen, grid, CELL_SIZE, CELL_DEAD, CELL_ALIVE)
+utils.draw_grid(screen, cell_x_cnt, cell_y_cnt,
+                GRID, CELL_SIZE, GRID_THICKNESS)
+
+running = True
+start = time.time()
+frame = 0
+while running:
+    cur = time.time()
+    if cur-start > 2:
+        pygame.display.set_caption(f"Game Of Life. FPS: {(frame / (cur-start)): .2f}")
+        start = time.time()
+        frame = 0
+
+    frame += 1
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEMOTION and pygame.mouse.get_pressed()[0] == 1:
+            utils.create_life(grid, CELL_SIZE)
+        elif event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0] == 1:
+            if utils.is_start_clicked(screen, CELL_SIZE):
+                EDITOR_MODE = not EDITOR_MODE
+                print(f'EDITOR_MODE={EDITOR_MODE}')
+
+    utils.fill_grid(screen, grid, CELL_SIZE, CELL_DEAD, CELL_ALIVE)
+    utils.draw_grid(screen, cell_x_cnt, cell_y_cnt,
+                    GRID, CELL_SIZE, GRID_THICKNESS)
+    if not EDITOR_MODE:
+        grid = utils.update(grid)
+    pygame.display.update()
+
+pygame.quit()
---
+++ utils.py
@@ -0,0 +1,63 @@
+import pygame
+import copy
+
+
+def fill_grid(screen: pygame.Surface, grid: list, cell_size: tuple, dead_color: tuple, alive_color: tuple):
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            pygame.draw.rect(screen, alive_color if grid[y][x] == 1 else dead_color, (x*cell_size, y*cell_size, cell_size, cell_size))
+
+
+def calc(grid: list, pos: tuple):
+    x, y = pos[0], pos[1]
+
+    count = 0
+    for i in range(max(0, y-1), min(len(grid), y+2)):
+        for j in range(max(0, x-1), min(len(grid[0]), x+2)):
+            if i == y and j == x:
+                continue
+            if grid[i][j] == 1:
+                count += 1
+
+    if grid[y][x] == 0 and count == 3:
+        return 1
+
+    if grid[y][x] == 1 and count in [2, 3]:
+        return 1
+    elif grid[y][x] == 1:
+        return 0
+
+    return 0
+
+
+def update(grid: list):
+    newgrd = copy.deepcopy(grid)
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            newgrd[y][x] = calc(grid, (x, y))
+    return newgrd
+
+
+def draw_grid(screen: pygame.Surface, x_cnt: int, y_cnt: int, grid_color: tuple, cell_size: int, thickness: int):
+    width, height = screen.get_size()
+    for c in range(x_cnt):
+        pygame.draw.line(screen, grid_color, (c*cell_size, 0),
+                        (c*cell_size, height), thickness)
+
+    for c in range(y_cnt):
+        pygame.draw.line(screen, grid_color, (0, c*cell_size),
+                        (width, c*cell_size), thickness)
+
+
+def create_life(grid: list, cell_size: int):
+    cx, cy = pygame.mouse.get_pos()
+    grid[cy//cell_size][cx//cell_size] = 1
+
+
+def is_start_clicked(screen: pygame.Surface, cell_size: int):
+    size = screen.get_size()
+    cx, cy = pygame.mouse.get_pos()
+
+    if size[0]//cell_size == cx//cell_size+1 and 0 == cy//cell_size:
+        return True
+    return False
